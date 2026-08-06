7fdc4296569d8c1217427187643cf90d60b31110
Sandesh Ghimire <sandesh@soccentric.com>
2026-08-05 19:00:26 -0700

    fix: .gitignore with proper line-separated patterns

--- .modules.order.cmd
+++
@@ -1,1 +0,0 @@
-savedcmd_modules.order := {   echo nexus-bus.o; :; } > modules.order
--- .nexus-bus.mod.cmd
+++
@@ -1,1 +0,0 @@
-savedcmd_nexus-bus.mod := printf '%s\n'   src/main.o src/bus_ioctl.o src/chardev.o | awk '!x[$$0]++ { print("./"$$0) }' > nexus-bus.mod
